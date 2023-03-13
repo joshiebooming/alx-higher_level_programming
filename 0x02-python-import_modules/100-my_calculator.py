6b44103590e0606ffd5579768529d56e97679027
joshiebooming <joashgamba@gmail.com>
2023-03-13 00:16:12 -0700

    today commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,27 @@
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
     import sys
+    nargs = len(sys.argv) -1
+    if nargs != 3:
+       print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+       sys.exit(1)
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    ops = sys.argv[2]
+    if ops != '+' and ops != '-' and ops != '*' and ops != '/':
+       print("Unknown operator. Available operators: +, -, * and /")
+       sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
+    from calculator_1 import add,sub,mul,div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+  
+    if ops == '+':
+       print("{} + {} = {}".format(a, b, add(a,b)))
+    elif ops =='-':
+       print("{} - {} = {}".format(a, b, sub(a,b)))
+    elif ops == '*':
+       print("{} * {} = {}".format(a, b, mul(a,b)))
+    else:
+       print("{} / {} = {}".format(a, b, div(a,b)))
+
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
 
     total = 0
 
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-        print("{}".format(total))
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            total += int(arg)
+    print(total)
